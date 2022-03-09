56eada3c2d87ae3bc91350cb6d3091341329d6f6
Frank <fsolleveld@outlook.com>
2022-03-09 11:55:55 +0100

    Initial Commit

---
+++ Primes/PrimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimesApp.swift
+//  Primes
+//
+//  Created by Frank Solleveld on 09/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct PrimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
