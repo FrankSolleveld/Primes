15dacc5c6b199472d83ced40b247ca28e741522a
Frank <fsolleveld@outlook.com>
2022-03-09 12:00:15 +0100

    removed self and changed the isPrime inner value

--- Primes/Views/IsPrimeModalView.swift
+++ Primes/Views/IsPrimeModalView.swift
@@ -4,19 +4,19 @@ struct IsPrimeModalView: View {
     @ObservedObject var store: Store<AppState, AppAction>
     var body: some View {
         VStack {
-            if isPrime(2) {
-                Text("\(self.store.value.count) is prime 🎉")
+            if isPrime(store.value.count) {
+                Text("\(store.value.count) is prime 🎉")
                 if self.store.value.favouritePrimes.contains(self.store.value.count) {
                     Button("Remove from favorite primes") {
-                        self.store.send(.primeModal(.removeFavouritePrimeTapped))
+                        store.send(.primeModal(.removeFavouritePrimeTapped))
                     }
                 } else {
                     Button("Save to favorite primes") {
-                        self.store.send(.primeModal(.saveFavouritePrimeTapped))
+                        store.send(.primeModal(.saveFavouritePrimeTapped))
                     }
                 }
             } else {
-                Text("\(self.store.value.count) is not prime :(")
+                Text("\(store.value.count) is not prime :(")
             }
         }
     }
