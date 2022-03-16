9ea7c09b3f8093d641bce9a934f76933164f14ee
Frank <fsolleveld@outlook.com>
2022-03-16 14:25:12 +0100

    extracted effects

---
+++ ComposableArchitecture/Effects.swift
@@ -0,0 +1,38 @@
+//
+//  Effects.swift
+//  ComposableArchitecture
+//
+//  Created by Frank Solleveld on 16/03/2022.
+//
+
+import Foundation
+
+public func dataTask(with url: URL) -> Effect<(Data?, URLResponse?, Error?)> {
+    return Effect { callback in
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            callback((data, response, error))
+        }
+        .resume()
+    }
+}
+
+public extension Effect where A == (Data?, URLResponse?, Error?) {
+    func decode<M: Decodable>(as type: M.Type) -> Effect<M?> {
+        return self.map { data, _, _ in
+            data
+                .flatMap { try? JSONDecoder().decode(M.self, from: $0) }
+        }
+    }
+}
+
+public extension Effect {
+    func receive(on queue: DispatchQueue) -> Effect {
+        return Effect { callback in
+            self.run { a in
+                queue.async {
+                    callback(a)
+                }
+            }
+        }
+    }
+}
--- Counter/WolframAlpha.swift
+++ Counter/WolframAlpha.swift
@@ -55,33 +55,3 @@ func wolframAlpha(query: String) -> Effect<WolframAlphaResult?> {
     return dataTask(with: components.url(relativeTo: nil)!)
         .decode(as: WolframAlphaResult.self)
 }
-
-func dataTask(with url: URL) -> Effect<(Data?, URLResponse?, Error?)> {
-    return Effect { callback in
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            callback((data, response, error))
-        }
-        .resume()
-    }
-}
-
-extension Effect where A == (Data?, URLResponse?, Error?) {
-    func decode<M: Decodable>(as type: M.Type) -> Effect<M?> {
-        return self.map { data, _, _ in
-            data
-                .flatMap { try? JSONDecoder().decode(M.self, from: $0) }
-        }
-    }
-}
-
-extension Effect {
-    func receive(on queue: DispatchQueue) -> Effect {
-        return Effect { callback in
-            self.run { a in
-                queue.async {
-                    callback(a)
-                }
-            }
-        }
-    }
-}
