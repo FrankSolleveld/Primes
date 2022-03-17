48482fac3e4ab8fe3d8a080bc58accd9587603b9
Frank <fsolleveld@outlook.com>
2022-03-17 10:45:14 +0100

    test prime modal

--- PrimeModalTests/PrimeModalTests.swift
+++ PrimeModalTests/PrimeModalTests.swift
@@ -9,28 +9,27 @@ import XCTest
 @testable import PrimeModal
 
 class PrimeModalTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testSaveFavouritePrimesTapped() {
+        var state = (count: 2, favouritePrimes: [3, 5])
+        let effects = primeModalReducer(
+            state: &state,
+            action: PrimeModalAction.saveFavouritePrimeTapped
+        )
+        let (count, favouritePrimes) = state
+        XCTAssertEqual(count, 2)
+        XCTAssertEqual(favouritePrimes, [3, 5, 2])
+        XCTAssert(effects.isEmpty)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testRemoveFavouritePrimesTapped() {
+        var state = (count: 2, favouritePrimes: [3, 5])
+        let effects = primeModalReducer(
+            state: &state,
+            action: .removeFavouritePrimeTapped
+        )
+        let (count, favouritePrimes) = state
+        XCTAssertEqual(count, 2)
+        XCTAssertEqual(favouritePrimes, [3, 5])
+        XCTAssert(effects.isEmpty)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
